58fe86174da4cd2f8bad6b510f208509bb20b4c3
Pryce Houck <phouck@ufl.edu>
2025-03-18 21:25:05 -0400

    leetcode progress in backend_main

--- get_to_work/backend/backend_main.py
+++ get_to_work/backend/backend_main.py
@@ -1,10 +1,16 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, APIRouter
+from leetcode_scraper import get_user_problems
 
 gtw = FastAPI()
+router = APIRouter()
 
 @gtw.get("/")
 async def root():
     return {"message": "test from app root"}
+
+@router.get("/leetcode/progress")
+def get_leetcode_progress():
+    return get_user_problems()
 
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
